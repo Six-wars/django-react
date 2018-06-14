1281556e6a9c9ddc5022a57a41b80a0889619a71
Samuel Muiruri <muiruri.samuel@gmail.com>
2018-06-14 14:54:18 +0300

    add jokes

--- comedy/jokes/models.py
+++ comedy/jokes/models.py
@@ -1,7 +1,19 @@
 from django.db import models
 
 class DadJokes(models.Model):
-	text = models.TextField()
+	text = models.TextField(unique=True)
+
+	def __str__(self):
+		return self.text
+
+class Tweet(models.Model):
+	text = models.TextField(unique=True)
+
+	def __str__(self):
+		return self.text
+
+class Joke(models.Model):
+	text = models.TextField(unique=True)
 
 	def __str__(self):
 		return self.text
